2eada2d9ea24bf6b039328be6b6181085f42187e
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-08-23 21:41:29 +0300

    Task2_lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,5 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
-import _ from 'lodash';
-import throttle from 'lodash.throttle/index';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -8,12 +7,9 @@ const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
 player.setCurrentTime(+localStorage.getItem(STORAGE_KEY));
 
-player.on('timeupdate', onTimeupdate);
+player.on('timeupdate', throttle(onTimeupdate, 1000));
 
 function onTimeupdate(params) {
   localStorage.setItem(STORAGE_KEY, params.seconds);
   console.log(params.seconds);
 }
-_.throttle(() => {
-  params.seconds;
-}, 1000);
