aa28b9f158427616ba9bb440e04a060670898977
BolterBait <osvyatobog@gmail.com>
2022-08-23 18:09:59 +0000

    Deploying to gh-pages from @ BolterBait/goit-js-hw-08@86f17ad20ba1759aff910ea45fe570fa82682ca7 🚀

--- 03-feedback.8704928f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t,a=document.querySelector(".feedback-form"),o=document.querySelector('[type="email"]'),l=document.querySelector('[name="message"]');e=localStorage.getItem("feedback-form-state"),t=JSON.parse(e),console.dir(t),console.log(e),t&&(o.value=t.email,l.value=t.message),a.addEventListener("input",(function(e){e.preventDefault();var t=e.target.form.elements,a=t.email,o=t.message,l={email:a.value,message:o.value};console.log(JSON.stringify(l)),localStorage.setItem("feedback-form-state",JSON.stringify(l))}))}();
-//# sourceMappingURL=03-feedback.8704928f.js.map
